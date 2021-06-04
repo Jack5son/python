3b6ac4c8f101978c002faa79c57611d855d14128
Jackson Luis <jackson-luis10@hotmail.com>
2021-06-04 19:16:33 -0300

    jogo

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -2,21 +2,29 @@ print("################################")
 print("Bem vindo ao jogo de adivinhação!")
 print("################################")
 
+# usuario tem 3 tentativas para execução
+tentativas = 5
 numero_secreto = 28
-chute = int (input("Digite o seu numero: "))
+chute = 0
 
-print("Você digitou:", chute)
+acertou = False
+maior = False
 
-acertou = chute == numero_secreto
-maior   = chute > numero_secreto
+while acertou == False and tentativas > 0:
+    chute = int (input("Digite o seu numero: "))
+    print("Você digitou:", chute)
+    acertou = chute == numero_secreto
+    maior   = chute > numero_secreto
 
-if (acertou):
-    print("Você acertou!!!")
-else:
-    print("Não foi dessa vez :(")
-    if(maior):
-        print("Tente um numero mais baixo!")
+    if (acertou):
+        print("Você acertou!!!")
     else:
-        print("Tente um numero mais alto")
+        tentativas = tentativas - 1
+        print("Não foi dessa vez :(")
+        if(maior):
+            print("Tente um numero mais baixo!")
+        else:
+            print("Tente um numero mais alto")
+        print("Restam" ,tentativas , "tentativas")
 
 print("Fim do jogo!")
